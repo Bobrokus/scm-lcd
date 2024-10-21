cbee8c0a0af09c7695bcaf525da566f56427f0ff
Bobo <63786893+Bobrokus@users.noreply.github.com>
2024-10-21 23:29:27 +0200

    noob eratosthenes implementation (fix later)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+/// Sieve of Eratosthenes
+fn eratosthenes(n: u32) -> Vec<u32> {
+    let mut numbers = (2..=n).collect::<Vec<u32>>();
+
+    let mut i = 0;
+
+    loop {
+        if let Some(prime) = numbers.get(i).cloned() {
+            numbers.retain(|&n| if n == prime { true } else { (n % prime != 0) });
+
+            i += 1;
+        } else {
+            break;
+        }
+    }
+
+    numbers
+}
+
+fn 
+
 fn main() {
-    println!("Hello, world!");
+    let x = 1;
+    let y = 2;
+
+    let primes = eratosthenes(f64::max(x, y).sqrt().ceil() as u32);
+
+    
 }

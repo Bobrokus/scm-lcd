57d3a059714b17ea3c8ae441871cc368ac540513
Bobo <63786893+Bobrokus@users.noreply.github.com>
2024-10-23 23:31:29 +0200

    + prime factorization (not affiliated with KSI)

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,40 @@
 /// Sieve of Eratosthenes
-fn eratosthenes(n: u32) -> Vec<u32> {
-    let mut numbers = (2..=n).collect::<Vec<u32>>();
+fn eratosthenes(n: u64) -> Vec<u64> {
+    let mut numbers = (2..=n).collect::<Vec<u64>>();
 
     let mut i = 0;
 
-    loop {
-        if let Some(prime) = numbers.get(i).cloned() {
-            numbers.retain(|&n| if n == prime { true } else { (n % prime != 0) });
+    while let Some(prime) = numbers.get(i).cloned() {
+        numbers.retain(|&n| n == prime || (n % prime) != 0);
 
-            i += 1;
-        } else {
-            break;
-        }
+        i += 1;
     }
 
     numbers
 }
 
-fn 
+fn prime_factorization(n: u64) -> Vec<u64> {
+    let sqrt_ceiled = (n as f64).sqrt().ceil() as u64;
+    let primes = eratosthenes(sqrt_ceiled);
+
+    for prime in primes {
+        if n == 2 {
+            return vec![n];
+        }
+
+        if n % prime == 0 {
+            let mut out = vec![];
+
+            out.push(prime);
+            out.append(&mut prime_factorization(n / prime));
+
+            return out;
+        }
+    }
+
+    vec![n]
+}
 
 fn main() {
-    let x = 1;
-    let y = 2;
-
-    let primes = eratosthenes(f64::max(x, y).sqrt().ceil() as u32);
-
-    
+    let a = dbg!(prime_factorization(50505));
 }
